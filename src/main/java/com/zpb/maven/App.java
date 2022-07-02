0507793c3fbac294f6cc24c2c9d8502c522067ba
zpb <2673404296@qq.com>
2022-07-02 17:20:52 +0800

    dev分支上添加内容

--- src/main/java/com/zpb/maven/App.java
+++ src/main/java/com/zpb/maven/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("在分支上添加内容");
     }
 }
